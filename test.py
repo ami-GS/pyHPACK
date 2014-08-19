be90b595c51c3535aa7f7365e42d4c9a9a61bd77
Daiki Aminaka <1991.daiki@gmail.com>
2014-08-19 22:53:32 +0900

    added encode tester

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
-import os
+import os, sys
 import json
-from HPACK import decode
+from HPACK import decode, encode
 
 
 TESTCASE = [
@@ -12,8 +12,33 @@ TESTCASE = [
     'hpack-test-case/haskell-http2-linear-huffman/',
 ]
 
+def encodeTest():
+    for i in range(len(TESTCASE)):
+        cases = [TESTCASE[i] + name for name in os.listdir(TESTCASE[i])]
+        break
+        for case in cases:
+            allPass = False
+            miss = False
+            with open(case) as f:
+                data = json.loads(f.read())
+                for seqno in range(len(data["cases"])):
+                    try:
+                        headers = [[h.keys()[0], h.values()[0]]for h in data["cases"][seqno]["headers"]]
+                    except Exception as e:
+                        print(e)
+                    code = encode(headers, False, False, False)
+                    if code != data["cases"][seqno][wire]:
+                        print("Missed in %s seqno %d" % (case, seqno))
+                        miss = True
+                        break
+                if miss:
+                    break
+                if seqno == len(data["cases"]) - 1:
+                    allPass = True
+            if allPass:
+                print('Passed the %s' % case)                        
 
-if __name__ == "__main__":
+def decodeTest():
     headers = None
     for i in range(len(TESTCASE)):
         cases = [TESTCASE[i] + name for name in os.listdir(TESTCASE[i])]
@@ -31,14 +56,18 @@ if __name__ == "__main__":
                         if header not in data['cases'][seqno]['headers']:
                             miss = True
                             print i, header
-                            #print headers[header.keys()[0]]
-                            print('Missed the in %s seqno %d' % (case, seqno))
-#                            break
+                            print('Missed in %s seqno %d' % (case, seqno))
                     if miss:
                         break
-#                    for header in data['cases'][seqno]['headers']:
-#                        if header not in headers:
                     if seqno == len(data['cases'])-1:
                         allPass = True
             if allPass:
                 print('Passed the %s' % case)
+
+
+if __name__ == "__main__":
+    args = sys.argv
+    if len(args) == 2 and args[1] == "-e":
+        encodeTest()
+    elif len(args) == 1 or (len(args) == 2 and args[1] == "-d"):
+        decodeTest()
