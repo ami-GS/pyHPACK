76fd64e77c7806ad818e99893ea35b6c6f48d1fa
Daiki Aminaka <1991.daiki@gmail.com>
2014-10-20 22:52:12 +0900

    added more test cases which using header table size

--- test.py
+++ test.py
@@ -3,6 +3,8 @@ import json
 from HPACK import decode, encode
 from tables import Table
 
+
+
 TESTCASE = [
     'hpack-test-case/haskell-http2-naive/',
     'hpack-test-case/haskell-http2-naive-huffman/',
@@ -10,6 +12,11 @@ TESTCASE = [
     'hpack-test-case/haskell-http2-static-huffman/',
     'hpack-test-case/haskell-http2-linear/',
     'hpack-test-case/haskell-http2-linear-huffman/',
+    'hpack-test-case/go-hpack/',
+    'hpack-test-case/nghttp2/',
+    'hpack-test-case/nghttp2-16384-4096/',
+    'hpack-test-case/nghttp2-change-table-size/',
+    'hpack-test-case/node-http2-hpack/'
 ]
 
 def encodeTest():
@@ -21,6 +28,9 @@ def encodeTest():
             with open(case) as f:
                 data = json.loads(f.read())
                 for seqno in range(len(data["cases"])):
+                    if data["cases"][seqno].has_key("header_table_size"):
+                        table.setMaxHeaderTableSize(int(data["cases"][seqno]["header_table_size"]))
+
                     headers = [[h.keys()[0], h.values()[0]]for h in data["cases"][seqno]["headers"]]
                     code = encode(headers, 'static' in case or 'linear' in case, 'linear' in case, 'huffman' in case, table) #init header table or not
                     if code != data["cases"][seqno]["wire"]:
@@ -42,6 +52,8 @@ def decodeTest():
             with open(case) as f:
                 data = json.loads(f.read())
                 for seqno in range(len(data['cases'])):
+                    if data["cases"][seqno].has_key("header_table_size"):
+                        table.setMaxHeaderTableSize(int(data["cases"][seqno]["header_table_size"]))
                     headers = decode(data['cases'][seqno]['wire'], table)
                     if headers != data['cases'][seqno]['headers']:
                         allPass = False
@@ -60,9 +72,14 @@ def encode2decode():
             with open(story) as f:
                 data = json.loads(f.read())
                 for seqno in range(len(data["cases"])):
+                    if data["cases"][seqno].has_key("header_table_size"):
+                        encoderTable.setMaxHeaderTableSize(int(data["cases"][seqno]["header_table_size"]))
+
                     headers = [[h.keys()[0], h.values()[0]] for h in data["cases"][seqno]["headers"]]
                     wire = encode(headers, 'static' in story or 'linear' in story, 'linear' in story, 'huffman' in story, encoderTable) #init header table or not
                     try:
+                        if data["cases"][seqno].has_key("header_table_size"):
+                            decoderTable.setMaxHeaderTableSize(int(data["cases"][seqno]["header_table_size"]))
                         decodedHeaders = decode(wire, decoderTable)
                         if decodedHeaders != data["cases"][seqno]["headers"]:
                             allPass = False
